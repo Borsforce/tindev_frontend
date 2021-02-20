ba3aef7f6ec438cb0219973f7369865c7f38df80
Kevin <xxkevtvxx@gmail.com>
2021-02-20 21:20:44 +0100

    feat: added Skill Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Skill title="Test" description="blablablablalbasl"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue';
+import Skill from '@/components/Skill.vue';
 
 export default {
   name: 'Home',
   components: {
+    Skill,
   },
 };
 </script>
