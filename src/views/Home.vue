9d1e0a967dc71c5d189cf1bc900c20dcbea6fd26
Özgür Isbert <oezguerisbert@gmail.com>
2021-02-20 23:04:04 +0100

    Tested VSpacer and HSpacer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <VSpacer />
+    <HSpacer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue';
+import VSpacer from '../components/VSpacer.vue';
+import HSpacer from '../components/HSpacer.vue';
 
 export default {
   name: 'Home',
-  components: {
-  },
+  components: { HSpacer, VSpacer },
 };
 </script>
