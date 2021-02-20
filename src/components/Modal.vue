6538545f9b77fcd0a8dfd133ce7985c86006f6df
Özgür Isbert <oezguerisbert@gmail.com>
2021-02-20 22:12:37 +0100

    feat: Added Modal feature. This commit fixes #2

---
+++ src/components/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container relative p-8 m-auto modal">
+    <div class="flex relative modalheader">
+      <div class="mr-auto items-start md:text-2xl">{{ this.$props.title }}</div>
+      <div class="cursor-pointer p-2">X</div>
+    </div>
+    <slot v-for="slot in this.$slots">{{ slot }}</slot>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Modal',
+  props: { title: { type: String, default: '' } },
+});
+</script>
+
+<style>
+.modal {
+  border-radius: 3rem;
+  background-color: #111;
+  color: white;
+}
+</style>
