2857a9747ae2d494cc6b2ec9326a319f8cf4df03
klarstil <klarstil@googlemail.com>
2021-02-20 22:37:34 +0100

    feat: icon component including auto loading closes #9

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Icon name="activity-log" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue';
+import Icon from '@/components/Icon.vue';
 
 export default {
   name: 'Home',
   components: {
+    Icon,
   },
 };
 </script>
