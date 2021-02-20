0130275fd10857378d7c4e6c7797f4430a5d3ea7
Özgür Isbert <oezguerisbert@gmail.com>
2021-02-20 21:47:53 +0100

    feat: Added FormGroup feature. This commit fixes #3

---
+++ src/components/FormGroup.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-full">
+    <div class="flex flex-start md:text-xl">{{ this.$props.title }}</div>
+    <slot v-for="slot in this.$slots">{{ slot }}</slot>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'FormGroup',
+  props: { title: { type: String, default: null } },
+});
+</script>
+
+<style></style>
