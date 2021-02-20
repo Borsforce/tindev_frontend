0a6647522ada8e440e119d3f165a151a6ea36df9
Kevin <xxkevtvxx@gmail.com>
2021-02-20 22:08:13 +0100

    feat: added InfoBox and Badge components

---
+++ src/components/InfoBox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <img :src="name" />
+    <p>{{ title }}</p>
+    <p>{{ subtitle }}</p>
+    <p>{{ description }}</p>
+    <a :href="link">link</a>
+    <div v-for="badge in badges" :key="badge">
+      <Badge :badge="badge.badge" :link="badge.link" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Badge from './Badge.vue';
+
+export default {
+  name: 'InfoBox',
+  props: {
+    image: String,
+    title: String,
+    subtitle: String,
+    description: String,
+    link: String,
+    badges: {
+      badge: String,
+      link: String,
+    },
+  },
+  components: {
+    Badge,
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <InfoBox image="Test" title="Test" subtitle="Test" description="Test" link="Test"
+    :badges="[{badge: 'Test1', link: 'Test1'},{badge: 'Test2', link: 'Test2'}]" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue';
+import InfoBox from '@/components/InfoBox.vue';
 
 export default {
   name: 'Home',
   components: {
+    InfoBox,
   },
 };
 </script>
