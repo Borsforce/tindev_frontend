8641d4c107caf90fd105b590fcaaf09225cb1e60
Özgür Isbert <oezguerisbert@gmail.com>
2021-02-20 22:46:31 +0100

    feat: Added (Horizontal / Vertical)Spacer Feature.

---
+++ src/components/HSpacer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hspacer"></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HSpacer',
+});
+</script>
+
+<style>
+.hspacer {
+  width: 100%;
+  height: 2rem;
+}
+</style>
---
+++ src/components/VSpacer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="vspacer"></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'VSpacer',
+});
+</script>
+
+<style>
+.vspacer {
+  width: 2rem;
+  height: 100%;
+}
+</style>
