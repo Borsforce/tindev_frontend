34f17f755094577a6739a0eba1ce43680f70621e
Kevin <xxkevtvxx@gmail.com>
2021-02-20 20:43:58 +0100

    imp: added Like and Comment placeholder and css

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="container mx-auto card rounded-lg">
     <img :src="image" class="rounded-full avatar" />
-    <p class="text-left title">{{ title }}</p>
-    <p class="text-left meta">{{ meta }}</p>+
+    <p>
+      <span class="text-left title">{{ title }}</span>
+      <span class="meta">{{ meta }}</span>
+    </p>
+    <p>
+      <span class="like" v-on:click="(() => console.log('Like!'))">Like</span>
+      <span class="comment" v-on:click="(() => console.log('Comment!'))">Comment</span>
+    </p>
   </div>
 </template>
 
@@ -22,6 +28,7 @@ export default {
   .card {
     background-color: #131315;
     color: #fff;
+    text-align: left;
   }
   .avatar {
     width: 53px;
@@ -40,9 +47,24 @@ export default {
   }
   .meta {
     padding-top: 16px;
+    padding-left: 9px;
     color: #717171;
     font-family: Poppins;
     font-size: 12px;
     line-height: 17px;
+  }
+  .like {
+    cursor: pointer;
+    width: 24px;
+    height: 24px;
+    padding-bottom: 15px;
+    padding-left: 55px;
+  }
+  .comment {
+    cursor: pointer;
+    width: 24px;
+    height: 24px;
+    padding-bottom: 15px;
+    padding-left: 28px;
   }
 </style>
