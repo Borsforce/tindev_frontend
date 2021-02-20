32dc39d9e4b1af877f78f49ff7857623a7e2e5c6
Cheety <cheety@cheety.de>
2021-02-20 21:22:23 +0100

    add Navbar Component

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,38 @@
 <template>
-  <div id="nav">
-    <router-link to="/">{{ $t('menu.home') }}</router-link>
-    <router-link to="/profile/">{{ $t('menu.profile') }}</router-link>
-    <router-link to="/job-finder/">{{ $t('menu.jobFinder') }}</router-link>
-    <router-link to="/projects/">{{ $t('menu.projects') }}</router-link>
-    <router-link to="/companies/">{{ $t('menu.companies') }}</router-link>
-    <router-link to="/messages/">{{ $t('menu.messages') }}</router-link>
-    <router-link to="/settings/">{{ $t('menu.settings') }}</router-link>
+  <div class="grid grid-cols-1 md:grid-cols-6 grid-flow-col">
+    <Navbar class="col-span-full md:col-span-1" />
+    <figure class="bg-gray-100 rounded-xl p-8 col-span-full md:col-span-5">
+      <img class="w-32 h-32" src="/sarah-dayan.jpg" alt="" width="384" height="512" />
+      <div class="pt-6 space-y-4">
+        <blockquote>
+          <p class="text-lg">
+            “Tailwind CSS is the only framework that I've seen scale on large teams. It’s easy to
+            customize, adapts to any design, and the build size is tiny.”
+          </p>
+        </blockquote>
+        <figcaption>
+          <div>
+            Sarah Dayan
+          </div>
+          <div>
+            Staff Engineer, Algolia
+          </div>
+        </figcaption>
+      </div>
+    </figure>
   </div>
-
-  <figure class="bg-gray-100 rounded-xl p-8">
-    <img class="w-32 h-32" src="/sarah-dayan.jpg" alt="" width="384" height="512" />
-    <div class="pt-6 space-y-4">
-      <blockquote>
-        <p class="text-lg">
-          “Tailwind CSS is the only framework that I've seen scale on large teams. It’s easy to
-          customize, adapts to any design, and the build size is tiny.”
-        </p>
-      </blockquote>
-      <figcaption>
-        <div>
-          Sarah Dayan
-        </div>
-        <div>
-          Staff Engineer, Algolia
-        </div>
-      </figcaption>
-    </div>
-  </figure>
   <router-view />
 </template>
+<script>
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+  },
+};
+
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="flex flex-col content-between text-left text-lg text-white p-12 text-white bg-navbar leading-6">
+    <router-link to="/" class='font-bold'>{{ $t('menu.home') }}</router-link>
+    <router-link to="/profile/" class='mt-7'>{{ $t('menu.profile') }}</router-link>
+    <router-link to="/job-finder/" class='mt-7'>{{ $t('menu.jobFinder') }}</router-link>
+    <router-link to="/projects/" class='mt-7'>{{ $t('menu.projects') }}</router-link>
+    <router-link to="/companies/" class='mt-7'>{{ $t('menu.companies') }}</router-link>
+    <router-link to="/messages/" class='mt-7'>{{ $t('menu.messages') }}</router-link>
+    <router-link to="/settings/" class='mt-7'>{{ $t('menu.settings') }}</router-link>
+  </nav>
+</template>
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+<style>
+.bg-navbar{
+  background-color: #181818;
+}
+</style>
