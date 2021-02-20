5befb956f7ffc25434262538a2c3f9cddffbff99
Kevin <xxkevtvxx@gmail.com>
2021-02-20 19:44:14 +0100

    feat: added Card Component

---
+++ src/components/Card.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container mx-auto card rounded-lg">
+    <img :src="image" class="rounded-full avatar" />
+    <p class="text-left title">{{ title }}</p>
+    <p class="text-left meta">{{ meta }}</p>+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    image: String,
+    title: String,
+    meta: String,
+    callback: Function,
+  },
+};
+</script>
+
+<style scoped>
+  .card {
+    background-color: #131315;
+    color: #fff;
+  }
+  .avatar {
+    width: 53px;
+    height: 53px;
+    position: absolute;
+    margin-top: -22px;
+    margin-left: -3px;
+  }
+  .title {
+    padding-top: 12px;
+    padding-left: 57px;
+    font-size: 18px;
+    font-family: Poppins;
+    font-weight: 700;
+    line-height: 25px;
+  }
+  .meta {
+    padding-top: 16px;
+    color: #717171;
+    font-family: Poppins;
+    font-size: 12px;
+    line-height: 17px;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Card image="https://yt3.ggpht.com/ytc/AAUvwnhBQAGf2nrKM3qY6B2LdZfGIkhKpjfYF87WF9Zj=s88-c-k-c0x00ffffff-no-rj"
+          title="Test"
+          meta="3 mins ago"
+          :callback="(() => console.log('test'))"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue';
+import Card from '@/components/Card.vue';
 
 export default {
   name: 'Home',
   components: {
+    Card,
   },
 };
 </script>
