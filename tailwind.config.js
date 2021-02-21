df0ad805dc7c1c7689fe745b6d2291a33ece7511
Nicole Wolf <nicole.wolf@8mylez.com>
2021-02-21 12:14:32 +0100

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 module.exports = {
   purge: [""],
   theme: {
-    fontFamily: {
-      'custom': ['Poppins', 'sans-serif'],
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        primary: '#9C5EEB',
+        secondary: '#1E2022',
+        gray: {
+          darkest: '#131315',
+          dark: '#181818',
+          default: '#474747',
+          light: '#5E5E5E',
+          lightest: '#B7B7B7',
+        }
+      }
     },
   },
   variants: {},
