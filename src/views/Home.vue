e18a03377f57d2117c534b2aeed0a4068e9a8e70
Nicole Wolf <nicole.wolf@goltfisch.de>
2021-02-23 12:32:42 +0100

    fix merge conflict

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,17 @@
 <template>
-<<<<<<< Updated upstream
   <div class="home">
-=======
-  <div class="home bg-dark">
-
->>>>>>> Stashed changes
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue';
-<<<<<<< Updated upstream
 import Icon from '@/components/Icon.vue';
-=======
-// import Icon from '@/components/Icon.vue';
-// import Button from '../components/Button.vue';
-// import IconButton from '../components/IconButton.vue';
->>>>>>> Stashed changes
 
 export default {
   name: 'Home',
   components: {
-<<<<<<< Updated upstream
     Icon,
-=======
-    // Button,
-    // IconButton,
-    // Icon,
->>>>>>> Stashed changes
   },
 };
 </script>
