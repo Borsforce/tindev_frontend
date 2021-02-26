1ae988d339dea0c37d7742addd097e4d5b779606
Yannick Fricke <yannickfricke@googlemail.com>
2021-02-26 02:25:13 +0100

    Removed Tailwind related stuff from the Button and IconButton

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,17 +1,13 @@
 <template>
   <button
     v-if="size === 'sm'"
-    v-bind:class="[variant === 'primary' ? 'bg-gray-900' : 'bg-purple-400']"
     type="button"
-    class="uppercase leading-tight text-white font-bold rounded-full py-2 px-7 items-center justify-center focus:outline-none"
   >
     <slot></slot>
   </button>
   <button
     v-if="size === 'lg'"
-    v-bind:class="[variant === 'primary' ? 'bg-gray-900' : 'bg-purple-400']"
     type="button"
-    class="text-2xl uppercase leading-tight text-white font-bold rounded-full py-3 px-8 items-center justify-center focus:outline-none"
   >
     <slot></slot>
   </button>
--- src/components/IconButton.vue
+++ src/components/IconButton.vue
@@ -1,17 +1,13 @@
 <template>
   <button
     v-if="size === 'sm'"
-    v-bind:class="[variant === 'primary' ? 'bg-gray-900' : 'bg-purple-400']"
     type="button"
-    class="text-white rounded p-1 items-center justify-center focus:outline-none"
   >
     <slot></slot>
   </button>
   <button
     v-if="size === 'lg'"
-    v-bind:class="[variant === 'primary' ? 'bg-gray-900' : 'bg-purple-400']"
     type="button"
-    class="text-white font-bold rounded-full p-4 items-center justify-center focus:outline-none"
   >
     <slot></slot>
   </button>
