6092f15304037839e15dd6fc54e18685355f2df5
Robin Kemna <kemna.robin@gmail.com>
2021-02-26 02:35:07 +0100

    [WIP]: login page

---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home"></div>
+
+  <Form>
+    <Input />
+    <Input />
+  </Form>
+</template>
+
+<script>
+import { Form, Input } from '../components';
+
+export default {
+  name: 'Login',
+  components: {
+    Form,
+    Input,
+  },
+};
+</script>
