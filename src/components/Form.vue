b6a4fbd9a6c9c3d425d9f5d8394992b3062d82cc
Özgür Isbert <oezguerisbert@gmail.com>
2021-02-27 00:15:04 +0100

    fix: Fixed the modal,form and formgroup component, added componentwise styling

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,16 +1,45 @@
 <template>
-  <form @submit.prevent="true" @submit.once="this.$props.onSubmit" class="w-full rounded-3xl">
-    <slot v-for="slot in this.$slots">{{ slot }}</slot>
-    <button type="submit" class="btn btn-primary">Submit</button>
+  <form class="form">
+    <FormGroup>
+      <slot v-for="slot in this.$slots">{{ slot }}</slot>
+    </FormGroup>
+    <div class="formoptions">
+      <button type="submit" class="formoption submitbutton">Submit</button>
+    </div>
   </form>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import FormGroup from './FormGroup.vue';
 
 export default defineComponent({
+  components: { FormGroup },
   name: 'Form',
-  props: { onSubmit: { type: Function, default: () => null } },
+  data() {
+    return {};
+  },
 });
 </script>
-<style></style>
+<style lang="scss">
+.form {
+  display: flex;
+  width: 100%;
+  flex-flow: row wrap;
+
+  .formoptions {
+    display: flex;
+    flex-flow: row-reverse wrap;
+    align-items: flex-end;
+    width: 100%;
+
+    .formoption {
+      align-self: flex-end;
+    }
+
+    .submitbutton {
+      margin-top: 2rem;
+    }
+  }
+}
+</style>
