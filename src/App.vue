b7884276bd331ac9e150071ae1585cfc4edf0229
Nicole Wolf <nicole.wolf@goltfisch.de>
2021-03-12 16:00:36 +0100

    add profile info box & styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="overview-page">
+  <div class="overview-page container">
     <div class='header'>
       <div class='overview-logo'>
         <img alt='Tindev' src='@/assets/logo.png' />
@@ -11,17 +11,27 @@
       </div>
     </div>
     <div class='sidebar'>
-      <router-link to="/">{{ $t('menu.home') }}</router-link>
-      <router-link to="/profile/">{{ $t('menu.profile') }}</router-link>
-      <router-link to="/job-finder/">{{ $t('menu.jobFinder') }}</router-link>
-      <router-link to="/projects/">{{ $t('menu.projects') }}</router-link>
-      <router-link to="/companies/">{{ $t('menu.companies') }}</router-link>
-      <router-link to="/messages/">{{ $t('menu.messages') }}</router-link>
-      <router-link to="/settings/">{{ $t('menu.settings') }}</router-link>
-      <router-view />
+      <div class='overview-profile-info'>
+        <div class='overview-profile-info--image'>
+          <img alt='Tindev' src='@/assets/profile-image.png' />
+        </div>
+        <div class='overview-profile-info--text'>
+          <h6> Joeel56</h6>
+          <span>Frontend Dev</span>
+        </div>
+      </div>
+      <div class='overview-sidebar--links'>
+        <router-link to="/">{{ $t('menu.home') }}</router-link>
+        <router-link to="/profile/">{{ $t('menu.profile') }}</router-link>
+        <router-link to="/job-finder/">{{ $t('menu.jobFinder') }}</router-link>
+        <router-link to="/projects/">{{ $t('menu.projects') }}</router-link>
+        <router-link to="/companies/">{{ $t('menu.companies') }}</router-link>
+        <router-link to="/messages/">{{ $t('menu.messages') }}</router-link>
+        <router-link to="/settings/">{{ $t('menu.settings') }}</router-link>
+        <router-view />
+      </div>
     </div>
     <div class='main'>
-      <h1>Hello</h1>
     </div>
     <div class='footer'>
       <img alt='Tindev' src='@/assets/logo.png' />
